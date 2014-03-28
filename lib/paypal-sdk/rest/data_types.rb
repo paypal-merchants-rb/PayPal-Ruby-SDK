577a98c8b2baeeb4fa55ffe996542b8e8c379316
siddick <mebramsha@paypal.com>
2014-03-28 12:11:34 +0530

    Support invoice APIs

--- lib/paypal-sdk/rest/data_types.rb
+++ lib/paypal-sdk/rest/data_types.rb
@@ -31,8 +31,20 @@ module PayPal::SDK
 
         def self.load_members
         end
+
+        class Number < Float
+        end
       end
 
+      class PaymentHistory < Base
+
+        def self.load_members
+          array_of  :payments, Payment
+          object_of :count, Integer
+          object_of :next_id, String
+        end
+
+      end
       class Payment < Base
 
         def self.load_members
@@ -137,6 +149,20 @@ module PayPal::SDK
           response = api.delete(path, {})
           self.merge!(response)
           success?
+        end
+
+        def update()
+          path = "v1/vault/credit-card/#{self.id}"
+          response = api.patch(path, self.to_hash, http_header)
+          self.merge!(response)
+          success?
+        end
+
+        class << self
+          def all(options = {})
+            path = "v1/vault/credit-card"
+            CreditCardHistory.new(api.get(path, options))
+          end
         end
 
       end
@@ -388,6 +414,7 @@ module PayPal::SDK
         def self.load_members
           object_of :id, String
           object_of :create_time, DateTime
+          object_of :update_time, DateTime
           object_of :amount, Amount
           object_of :state, String
           object_of :sale_id, String
@@ -415,15 +442,6 @@ module PayPal::SDK
         end
 
       end
-      class PaymentHistory < Base
-
-        def self.load_members
-          array_of  :payments, Payment
-          object_of :count, Integer
-          object_of :next_id, String
-        end
-
-      end
       class PaymentExecution < Base
 
         def self.load_members
@@ -445,6 +463,407 @@ module PayPal::SDK
           array_of  :"credit-cards", CreditCard
           object_of :count, Integer
           object_of :next_id, String
+        end
+
+      end
+      class Error < Base
+
+        def self.load_members
+          object_of :name, String
+          object_of :debug_id, String
+          object_of :message, String
+          object_of :information_link, String
+          array_of  :details, ErrorDetails
+        end
+
+      end
+      class Invoice < Base
+
+        def self.load_members
+          object_of :id, String
+          object_of :number, String
+          object_of :uri, String
+          object_of :status, String
+          object_of :merchant_info, MerchantInfo
+          array_of  :billing_info, BillingInfo
+          object_of :shipping_info, ShippingInfo
+          array_of  :items, InvoiceItem
+          object_of :invoice_date, String
+          object_of :payment_term, PaymentTerm
+          object_of :discount, Cost
+          object_of :shipping_cost, ShippingCost
+          object_of :custom, CustomAmount
+          object_of :tax_calculated_after_discount, Boolean
+          object_of :tax_inclusive, Boolean
+          object_of :terms, String
+          object_of :note, String
+          object_of :merchant_memo, String
+          object_of :logo_url, String
+          object_of :total_amount, Currency
+          array_of  :payment_details, PaymentDetail
+          array_of  :refund_details, RefundDetail
+          object_of :metadata, Metadata
+        end
+
+        include RequestDataType
+
+        def create()
+          path = "v1/invoicing/invoices"
+          response = api.post(path, self.to_hash, http_header)
+          self.merge!(response)
+          success?
+        end
+
+        class << self
+          def self.search(options)
+            path = "v1/invoicing/search"
+            response = api.post(path, options)
+            Invoices.new(response)
+          end
+        end
+
+        def send()
+          path = "v1/invoicing/invoices/#{self.id}/send"
+          response = api.post(path, {}, http_header)
+          self.merge!(response)
+          success?
+        end
+
+        def remind(notification)
+          notification = Notification.new(notification) unless notification.is_a? Notification
+          path = "v1/invoicing/invoices/#{self.id}/remind"
+          response = api.post(path, notification.to_hash, http_header)
+          self.merge!(response)
+          success?
+        end
+
+        def cancel(cancel_notification)
+          cancel_notification = CancelNotification.new(cancel_notification) unless cancel_notification.is_a? CancelNotification
+          path = "v1/invoicing/invoices/#{self.id}/cancel"
+          response = api.post(path, cancel_notification.to_hash, http_header)
+          self.merge!(response)
+          success?
+        end
+
+        def record_payment(payment_detail)
+          payment_detail = PaymentDetail.new(payment_detail) unless payment_detail.is_a? PaymentDetail
+          path = "v1/invoicing/invoices/#{self.id}/record-payment"
+          response = api.post(path, payment_detail.to_hash, http_header)
+          self.merge!(response)
+          success?
+        end
+
+        def record_refund(refund_detail)
+          refund_detail = RefundDetail.new(refund_detail) unless refund_detail.is_a? RefundDetail
+          path = "v1/invoicing/invoices/#{self.id}/record-refund"
+          response = api.post(path, refund_detail.to_hash, http_header)
+          self.merge!(response)
+          success?
+        end
+
+        class << self
+          def find(resource_id)
+            raise ArgumentError.new("id required") if resource_id.to_s.strip.empty?
+            path = "v1/invoicing/invoices/#{resource_id}"
+            self.new(api.get(path))
+          end
+        end
+
+        class << self
+          def get_all(options = {})
+            path = "v1/invoicing/invoices/"
+            Invoices.new(api.get(path, options))
+          end
+        end
+
+        def update()
+          path = "v1/invoicing/invoices/#{self.id}"
+          response = api.put(path, self.to_hash, http_header)
+          self.merge!(response)
+          success?
+        end
+
+        def delete()
+          path = "v1/invoicing/invoices/#{self.id}"
+          response = api.delete(path, {})
+          self.merge!(response)
+          success?
+        end
+
+      end
+      class Invoices < Base
+
+        def self.load_members
+          object_of :total_count, Integer
+          array_of  :invoices, Invoice
+        end
+
+      end
+      class InvoiceItem < Base
+
+        def self.load_members
+          object_of :name, String
+          object_of :description, String
+          object_of :quantity, Number
+          object_of :unit_price, Currency
+          object_of :tax, Tax
+          object_of :date, String
+          object_of :discount, Cost
+        end
+
+      end
+      class MerchantInfo < Base
+
+        def self.load_members
+          object_of :email, String
+          object_of :first_name, String
+          object_of :last_name, String
+          object_of :address, Address
+          object_of :business_name, String
+          object_of :phone, Phone
+          object_of :fax, Phone
+          object_of :website, String
+          object_of :tax_id, String
+          object_of :additional_info, String
+        end
+
+      end
+      class BillingInfo < Base
+
+        def self.load_members
+          object_of :email, String
+          object_of :first_name, String
+          object_of :last_name, String
+          object_of :business_name, String
+          object_of :address, Address
+          object_of :language, String
+          object_of :additional_info, String
+        end
+
+      end
+      class ShippingInfo < Base
+
+        def self.load_members
+          object_of :first_name, String
+          object_of :last_name, String
+          object_of :business_name, String
+          object_of :address, Address
+        end
+
+      end
+      class InvoicingNotification < Base
+
+        def self.load_members
+          object_of :subject, String
+          object_of :note, String
+          object_of :send_to_merchant, Boolean
+        end
+
+      end
+      class InvoicingMetaData < Base
+
+        def self.load_members
+          object_of :created_date, String
+          object_of :created_by, String
+          object_of :cancelled_date, String
+          object_of :cancelled_by, String
+          object_of :last_updated_date, String
+          object_of :last_updated_by, String
+          object_of :first_sent_date, String
+          object_of :last_sent_date, String
+          object_of :last_sent_by, String
+        end
+
+      end
+      class InvoicingPaymentDetail < Base
+
+        def self.load_members
+          object_of :type, String
+          object_of :transaction_id, String
+          object_of :transaction_type, String
+          object_of :date, String
+          object_of :method, String
+          object_of :note, String
+        end
+
+      end
+      class InvoicingRefundDetail < Base
+
+        def self.load_members
+          object_of :type, String
+          object_of :date, String
+          object_of :note, String
+        end
+
+      end
+      class InvoicingSearch < Base
+
+        def self.load_members
+          object_of :email, String
+          object_of :recipient_first_name, String
+          object_of :recipient_last_name, String
+          object_of :recipient_business_name, String
+          object_of :number, String
+          object_of :status, String
+          object_of :lower_total_amount, Currency
+          object_of :upper_total_amount, Currency
+          object_of :start_invoice_date, String
+          object_of :end_invoice_date, String
+          object_of :start_due_date, String
+          object_of :end_due_date, String
+          object_of :start_payment_date, String
+          object_of :end_payment_date, String
+          object_of :start_creation_date, String
+          object_of :end_creation_date, String
+          object_of :page, Number
+          object_of :page_size, Number
+          object_of :total_count_required, Boolean
+        end
+
+      end
+      class ErrorDetails < Base
+
+        def self.load_members
+          object_of :field, String
+          object_of :issue, String
+        end
+
+      end
+      class PaymentTerm < Base
+
+        def self.load_members
+          object_of :term_type, String
+          object_of :due_date, String
+        end
+
+      end
+      class Cost < Base
+
+        def self.load_members
+          object_of :percent, Number
+          object_of :amount, Currency
+        end
+
+      end
+      class Currency < Base
+
+        def self.load_members
+          object_of :currency, String
+          object_of :value, String
+        end
+
+      end
+      class ShippingCost < Base
+
+        def self.load_members
+          object_of :amount, Currency
+          object_of :tax, Tax
+        end
+
+      end
+      class Tax < Base
+
+        def self.load_members
+          object_of :id, String
+          object_of :name, String
+          object_of :percent, Number
+          object_of :amount, Currency
+        end
+
+      end
+      class CustomAmount < Base
+
+        def self.load_members
+          object_of :label, String
+          object_of :amount, Currency
+        end
+
+      end
+      class PaymentDetail < Base
+
+        def self.load_members
+          object_of :type, String
+          object_of :transaction_id, String
+          object_of :transaction_type, String
+          object_of :date, String
+          object_of :method, String
+          object_of :note, String
+        end
+
+      end
+      class RefundDetail < Base
+
+        def self.load_members
+          object_of :type, String
+          object_of :date, String
+          object_of :note, String
+        end
+
+      end
+      class Metadata < Base
+
+        def self.load_members
+          object_of :created_date, String
+          object_of :created_by, String
+          object_of :cancelled_date, String
+          object_of :cancelled_by, String
+          object_of :last_updated_date, String
+          object_of :last_updated_by, String
+          object_of :first_sent_date, String
+          object_of :last_sent_date, String
+          object_of :last_sent_by, String
+        end
+
+      end
+      class Phone < Base
+
+        def self.load_members
+          object_of :country_code, String
+          object_of :national_number, String
+        end
+
+      end
+      class Notification < Base
+
+        def self.load_members
+          object_of :subject, String
+          object_of :note, String
+          object_of :send_to_merchant, Boolean
+        end
+
+      end
+      class Search < Base
+
+        def self.load_members
+          object_of :email, String
+          object_of :recipient_first_name, String
+          object_of :recipient_last_name, String
+          object_of :recipient_business_name, String
+          object_of :number, String
+          object_of :status, String
+          object_of :lower_total_amount, Currency
+          object_of :upper_total_amount, Currency
+          object_of :start_invoice_date, String
+          object_of :end_invoice_date, String
+          object_of :start_due_date, String
+          object_of :end_due_date, String
+          object_of :start_payment_date, String
+          object_of :end_payment_date, String
+          object_of :start_creation_date, String
+          object_of :end_creation_date, String
+          object_of :page, Number
+          object_of :page_size, Number
+          object_of :total_count_required, Boolean
+        end
+
+      end
+      class CancelNotification < Base
+
+        def self.load_members
+          object_of :subject, String
+          object_of :note, String
+          object_of :send_to_merchant, Boolean
+          object_of :send_to_payer, Boolean
         end
 
       end
