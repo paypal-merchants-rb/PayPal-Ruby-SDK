e03af267c54810ee25e60dc244eecc6bb0de8216
juwon <juwlee@paypal.com>
2014-12-15 14:55:43 -0600

    add more WebProfile tests

--- spec/web_profile_examples_spec.rb
+++ spec/web_profile_examples_spec.rb
@@ -23,9 +23,10 @@ describe "WebProfiles" do
   describe "Examples" do
     describe "WebProfile", :integration => true do
       it "Create" do
-        $webprofile = WebProfile.new(WebProfileAttributes)
-        webprofile.create
-        expect(webprofile.name.to_s).to eq("YeowZa! T-Shirt Shop")
+        api = API.new
+        $webprofile = WebProfile.new(WebProfileAttributes.merge( :token => api.token ))
+        $webprofile.create
+        expect($webprofile.name.to_s).to eq("YeowZa! T-Shirt Shop")
       end
 
       it "List" do
@@ -55,11 +56,37 @@ describe "WebProfiles" do
         expect(webprofile.name).to eq("YeowZa! T-Shirt Shop-8871170336226187544")
       end
 
+      it "Partial update" do
+        # retrieve web profile to perform partial update on
+        webprofile = WebProfile.find("XP-A4UX-5GLG-DAA8-26FL")
+
+        # set up partial update
+        h = {"op" => "replace",
+          "path" => "/presentation/brand_name",
+          "value" => "new_brand_name"}
+
+        # do partial update by sending a patch request
+        expect(webprofile.partial_update( [h] )).to be_truthy
+        webprofile = WebProfile.find("XP-A4UX-5GLG-DAA8-26FL")
+        expect(webprofile.presentation.brand_name).to eq("new_brand_name")
+
+        # restore original value for the next test run
+        h = {"op" => "replace",
+          "path" => "/presentation/brand_name",
+          "value" => "brand_name"}
+        expect(webprofile.partial_update( [h] )).to be_truthy
+        expect(WebProfile.find("XP-A4UX-5GLG-DAA8-26FL").presentation.brand_name).to eq("brand_name")
+      end
+
       it "Delete" do
-        $webprofile.delete
+        puts $webprofile.inspect
+        # delete newly created web profile from above create test
+        expect($webprofile.delete).to be_truthy
+
+        # make sure it is not retrieved from the system
         webprofile = WebProfile.find($webprofile.id)
         puts webprofile
-        expect(webprofile).to be None
+        expect(webprofile).to be_nil
       end
     end
   end
