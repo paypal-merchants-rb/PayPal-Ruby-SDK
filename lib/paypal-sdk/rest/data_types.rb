a87a9972c4ac3e480734f1d5721f6879ef6f28c4
Philip Tolton <mrtolton@gmail.com>
2015-08-05 17:16:24 -0400

    fix payments

--- lib/paypal-sdk/rest/data_types.rb
+++ lib/paypal-sdk/rest/data_types.rb
@@ -941,6 +941,7 @@ module PayPal::SDK
           object_of :logo_url, String
           object_of :total_amount, Currency
           array_of  :payment_details, PaymentDetail
+          array_of  :payments, PaymentDetail
           array_of  :refund_details, RefundDetail
           object_of :metadata, Metadata
         end
