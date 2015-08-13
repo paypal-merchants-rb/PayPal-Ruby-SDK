8ef6f657bcf6997872cd65c7c96a1f177638afe4
juwon <juwlee@paypal.com>
2015-08-13 17:20:36 -0500

    add OpenID connect test

--- spec/core/openid_connect_spec.rb
+++ spec/core/openid_connect_spec.rb
@@ -131,10 +131,18 @@ describe PayPal::SDK::OpenIDConnect do
       OpenIDConnect::Userinfo.api.stub( :post => { :name => "Testing" } )
 
       userinfo = OpenIDConnect::Userinfo.get("access_token")
+      userinfo = OpenIDConnect::Userinfo.new( { :name => "Testing" } )
       userinfo.should be_a OpenIDConnect::Userinfo
       userinfo.name.should eql "Testing"
 
       userinfo = OpenIDConnect::Userinfo.get( :access_token => "access_token" )
+      userinfo.should be_a OpenIDConnect::Userinfo
+      userinfo.name.should eql "Testing"
+    end
+
+    it "get", :integration => true do
+      api = OpenIDConnect::Userinfo.api.new( { :name => "Testing" } )
+      userinfo = OpenIDConnect::Userinfo.get(api.access_token)
       userinfo.should be_a OpenIDConnect::Userinfo
       userinfo.name.should eql "Testing"
     end
